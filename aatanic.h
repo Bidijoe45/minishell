889bcb01835622231c695adb6f05537d66b4b7d0
Adrian nicusor Pavel <apavel@c1r5s2.42madrid.com>
2020-11-23 14:16:19 +0100

    leo messi

---
+++ aatanic.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aatanic.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apavel <apavel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/23 13:59:53 by apavel            #+#    #+#             */
+/*   Updated: 2020/11/23 14:06:39 by apavel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AATANIC_H
+# define AATANIC_H
+# define NAME "aatanic"
+# include "./get_next_line/get_next_line.h"
+# include "./libft/libft.h"
+# include <unistd.h>
+#endif
