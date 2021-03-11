7fa4c100c314dd6e5afd6076f68c68777fbbc73f
alvrodri <alvaromarianorodriguezbenitez@gmail.com>
2021-03-11 16:35:12 +0100

    Fix double input

--- print/print.c
+++ print/print.c
@@ -6,7 +6,7 @@
 /*   By: alvrodri <alvrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 10:31:58 by alvrodri          #+#    #+#             */
-/*   Updated: 2020/11/30 12:52:33 by alvrodri         ###   ########.fr       */
+/*   Updated: 2021/03/11 15:01:09 by alvrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,4 @@ void	ft_print_error(t_fresh *fresh, char *error_str)
     ft_print_color(RED, "Error: ");
     ft_print_color(RESET, error_str);
 	ft_print_color(RESET, "\n");
-	ft_print_input(fresh);
 }
